5a8c5108af96724dfe26c308e73ac9fa5574b355
Shadowdx24 <110624444+Shadowdx24@users.noreply.github.com>
2024-02-07 18:52:39 +0530

    Added Functionality for Camera Movement and Created a Platform in Level 1

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private Transform cameraTarget;
+    private Vector2 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position-cameraTarget.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3 (cameraTarget.position.x,cameraTarget.position.y,transform.position.z);
+    }
+}
