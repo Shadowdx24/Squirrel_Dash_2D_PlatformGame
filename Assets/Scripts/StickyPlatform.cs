e0cca1907276d744b29f299b4fd0295fecc962d7
Shadowdx24 <110624444+Shadowdx24@users.noreply.github.com>
2024-02-29 18:42:53 +0530

    Added Functionality Moving Platform

---
+++ Assets/Scripts/StickyPlatform.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class StickyPlatform : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.transform.SetParent(transform);
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.transform.SetParent(null);
+        }
+    }
+}
