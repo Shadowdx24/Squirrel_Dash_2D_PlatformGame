cf108a778562cf17c3f1409c157e1775e4edff00
Shadowdx24 <nanavatiarpen45@gmail.com>
2024-04-23 18:52:00 +0530

    Modified  Level choice scene

--- Assets/Scripts/Home.cs
+++ Assets/Scripts/Home.cs
@@ -3,18 +3,56 @@ using UnityEngine.SceneManagement;
 
 public class Home : MonoBehaviour
 {
+    [SerializeField] private GameObject LevelObj;
     public void GameStart()
     {
         // Start a Game
-        SceneManager.LoadScene(1);
-        AudioManager.instance.Play("Level 1");
-        AudioManager.instance.Stop("Home");
-        PlayerPrefs.SetInt("Health",3);
+        GameLevel();
+        //SceneManager.LoadScene(1);
+       // AudioManager.instance.Play("Level 1");
+       //AudioManager.instance.Stop("Home");
+       //PlayerPrefs.SetInt("Health",3);
     }
 
     public void GameQuit()
     {
         // Quit a Game
         Application.Quit();
+    }
+
+    private void GameLevel()
+    {
+        LevelObj.SetActive(true);
+    }
+
+    public void Level1()
+    {
+        SceneManager.LoadScene(1);
+        AudioManager.instance.Play("Level 1");
+        AudioManager.instance.Stop("Home");
+        PlayerPrefs.SetInt("Health", 3);
+    }
+    public void Level2()
+    {
+        SceneManager.LoadScene(2);
+        AudioManager.instance.Play("Level 2");
+        AudioManager.instance.Stop("Home");
+        PlayerPrefs.SetInt("Health", 3);
+    }
+
+    public void Level3()
+    {
+        SceneManager.LoadScene(3);
+        AudioManager.instance.Play("Level 3");
+        AudioManager.instance.Stop("Home");
+        PlayerPrefs.SetInt("Health", 3);
+    }
+
+    public void Level4()
+    {
+        SceneManager.LoadScene(4);
+        //AudioManager.instance.Play("Level 1");
+        AudioManager.instance.Stop("Home");
+        PlayerPrefs.SetInt("Health", 3);
     }
 }
