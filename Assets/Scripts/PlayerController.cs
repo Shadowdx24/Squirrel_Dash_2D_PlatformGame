e80135fd418791f8a76eece33efbd4a56e840fcc
Shadowdx24 <nanavatiarpen45@gmail.com>
2024-05-17 18:24:37 +0530

    Modified Functionality for Boss Bullet

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -20,7 +20,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private TextMeshProUGUI healthText;
     [SerializeField] private GameObject gameOverObj;
     [SerializeField] private GameObject gamePauseObj;
-    
+    [SerializeField] private Animator BulletAnimator;
+
     void Start()
     {
        // currHeath = maxHealth;
@@ -113,6 +114,8 @@ public class PlayerController : MonoBehaviour
             // Die to A Player with Fall
             currHeath--;
             PlayerPrefs.SetInt("Health", currHeath);
+            //BulletAnimator.SetTrigger("explod");
+            collision.gameObject.GetComponent<Animator>().SetTrigger("explod");
             PlayerAnimator.SetTrigger("Die");
             AudioManager.instance.Play("Hurt");
 
